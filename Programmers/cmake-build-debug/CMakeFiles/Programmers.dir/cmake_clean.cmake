603a1f8d5a1fcbe01459dc4be472295ade872e3f
ellyheetov <60229909+ellyheetov@users.noreply.github.com>
2020-04-25 01:16:53 +0900

    Create Programmers folder

---
+++ Programmers/cmake-build-debug/CMakeFiles/Programmers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programmers.dir/42576_완주하지_못한_선수.cpp.o"
+  "Programmers"
+  "Programmers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Programmers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
