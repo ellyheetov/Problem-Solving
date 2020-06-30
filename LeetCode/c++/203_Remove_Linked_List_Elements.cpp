c12dd8b780e265b27b232af5e0a6af17f9123d2c
ellyheetov <yaaajcm@naver.com>
2020-06-30 19:19:39 +0900

    solved 3 problems linked list

---
+++ LeetCode/c++/203_Remove_Linked_List_Elements.cpp
@@ -0,0 +1,116 @@
+//
+// Created by 박혜원 on 2020/06/30.
+//
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *removeElements(ListNode *head, int val) {
+        while(head!=NULL){
+            if(head->val == val)
+                head = head->next;
+            else
+                break;
+        }
+        ListNode *cur = head;
+        while(cur != NULL && cur->next != NULL){
+            if(cur->next->val == val)
+                cur->next = cur->next->next;
+            else
+                cur = cur->next;
+        }
+        return head;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode *stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode *dummyRoot = new ListNode(0);
+    ListNode *ptr = dummyRoot;
+    for (int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string listNodeToString(ListNode *node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode *head = stringToListNode(line);
+        getline(cin, line);
+        int val = stringToInteger(line);
+
+        ListNode *ret = Solution().removeElements(head, val);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
