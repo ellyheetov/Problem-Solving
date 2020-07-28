3bb64f3e074a8c3f14c8374445da3481cc7c1284
ellyheetov <yaaajcm@naver.com>
2020-07-28 19:49:05 +0900

    refactor: Leetcode 1.Two Sum
    Using one-pass Hash Table
    
    Time Complexity: O(N)
    Space Complexity: O(N)

--- LeetCode/Java/src/Leetcode1_Two_Sum.java
+++ LeetCode/Java/src/Leetcode1_Two_Sum.java
@@ -1,13 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Leetcode1_Two_Sum {
     public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
+
+        Map<Integer,Integer> map  = new HashMap<>();
+
+        for(int i=0;i< nums.length;i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement))
+                return new int[] { map.get(complement), i};
+            map.put(nums[i],i);
         }
-        throw new IllegalArgumentException("No Two Sum solution");
+        throw new IllegalArgumentException("No two sum solution");
     }
 
     public static void main(String[] args) {
