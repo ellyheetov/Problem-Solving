2558682238f07f0f7f376bcc30aeb64f1db548bf
ellyheetov <yaaajcm@naver.com>
2020-08-07 17:37:26 +0900

    refactor: retry Leetocde 53. Maximum Subarray
    Time Complexity: O(N)
    Space Complexity : O(1)

--- LeetCode/Java/src/Leetcode53_Maximum_Subarray.java
+++ LeetCode/Java/src/Leetcode53_Maximum_Subarray.java
@@ -1,25 +1,24 @@
 public class Leetcode53_Maximum_Subarray {
 
     public static int maxSubArray(int[] nums) {
-        if(nums.length < 1) return 0;
-        int max_value = nums[0];
+        if (nums.length < 1) return 0;
+        int maxValue = nums[0];
         int current = Math.max(nums[0], 0);
 
-        for(int i = 1;i< nums.length; i++){
+        for (int i = 1; i < nums.length; i++) {
             current = current + nums[i];
-            if(max_value < current)
-                max_value = current;
-            if(current< 0)
+            maxValue = Math.max(maxValue, current);
+            if (current < 0)
                 current = 0;
         }
-        return max_value;
+        return maxValue;
     }
 
     public static void main(String[] args) {
         // write your code here
-        int[] nums = {1,2};
-        int answer= maxSubArray(nums);
-        System.out.println(answer);
+        int[] nums = {1, 2};
+        int answer = maxSubArray(nums);
+        System.out.print(answer);
     }
 }
 
