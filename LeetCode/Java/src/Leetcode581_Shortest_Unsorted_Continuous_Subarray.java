cf2ebd5e1918d4b3c07c6dffb8bdbc4b874f9aaa
ellyheetov <yaaajcm@naver.com>
2020-11-07 21:58:20 +0900

    Leetcode 581. Shortest Unsorted Continuous Subarray
    Stack, Index

--- LeetCode/Java/src/Leetcode581_Shortest_Unsorted_Continuous_Subarray.java
+++ LeetCode/Java/src/Leetcode581_Shortest_Unsorted_Continuous_Subarray.java
@@ -1,52 +1,52 @@
-import java.util.Arrays;
+import java.util.Stack;
+
+/*
+문제 : 오름차순으로 정렬된 배열을 얻기 위해서 정렬이 필요한 부분의 길이를 구한다.
+
+Index와 Stack을 활용하여 문제 풀이
+
+Step 1. 정렬이 필요한 시작 index 구하기
+주어진 배열의 앞에서부터 탐색한다.
+증가 하는 값일 경우 stack에 넣는다.
+증가 하지 않는 값일 경우 stack에 있는 값과 비교하여 조건이 맞을 때까지 stack에서 pop한다.
+
+Step 2. 정렬이 필요한 마지막 index 구하기
+주어진 배열의 뒤에서부터 탐색한다.
+감소하는 값일 경우 stack에 넣는다.
+증가하는 경우에는 stack에 있는 값과 비교하여 조건이 맞을 때까지 stack에서 pop한다.
+
+Step 3. 길이를 반환
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+ */
 
 public class Leetcode581_Shortest_Unsorted_Continuous_Subarray {
     public static int findUnsortedSubarray(int[] nums) {
-
-        int size = nums.length;
-        int[] copyArray = new int[size];
-        int answer = 0;
-
-        System.arraycopy(nums, 0, copyArray, 0, size);
-        Arrays.sort(copyArray);
+        Stack<Integer> stack = new Stack<>();
 
         int start = nums.length;
-        int end = 0;
+        int last = 0;
 
-//        int start = findStartIndex(nums, copyArray);
-//        int end = findEndIndex(nums, copyArray);
+        for (int index = 0; index < nums.length; index++) {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[index])
+                start = Math.min(start, stack.pop());
+            stack.push(index);
+        }
+        stack.clear();
 
-        for(int index = 0; index < nums.length; index++){
-            if(nums[index] != copyArray[index]){
-                start = Math.min(start, index);
-                end = Math.max(end, index);
+        for (int index = nums.length - 1; index >= 0; index--) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[index]) {
+                last = Math.max(last, stack.pop());
             }
+            stack.push(index);
         }
-        answer = end - start + 1;
-        if(end == 0) return 0;
-        return answer;
-    }
 
-/*    private static int findEndIndex(int[] nums, int[] copyArray) {
-        int index = nums.length - 1;
-        while (index >= 0) {
-            if (nums[index] != copyArray[index]) break;
-            index--;
-        }
-        return index;
+        return last - start > 0 ? last - start + 1 : 0;
     }
-
-    private static int findStartIndex(int[] nums, int[] copyArray) {
-        int index = 0;
-        while (index < nums.length) {
-            if (nums[index] != copyArray[index]) break;
-            index++;
-        }
-        return index;
-    }*/
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4};
+        int[] nums = {2, 6, 4, 8, 15, 9, 10};
         int answer = findUnsortedSubarray(nums);
         System.out.print(answer);
     }
